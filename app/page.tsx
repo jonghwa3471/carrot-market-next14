fc8d01a4fbd22f1df5baa7277cf9d82c8b435444
Jonghwa <whdghk3471@gmail.com>
2025-09-05 17:30:59 +0900

    #3.6 Form Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ export default function Home() {
     <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg md:flex-row">
         <input
-          className="h-10 w-full rounded-full bg-gray-200 pl-5 outline-none ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-gray-500 focus:ring-offset-2"
-          type="text"
-          placeholder="Search here..."
+          className="peer h-10 w-full rounded-full bg-gray-200 pl-5 outline-none ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500"
+          type="email"
+          placeholder="Email address"
+          required
         />
+        <span className="hidden font-medium text-red-500 peer-invalid:block">
+          Email is required.
+        </span>
         <button className="rounded-full bg-black py-2 font-medium text-white outline-none transition-transform active:scale-90 md:px-10">
-          Search
+          Log in
         </button>
       </div>
     </main>
