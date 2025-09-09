148ca7ddf8dc65c666d430387fdcd02166922b6f
Jonghwa <whdghk3471@gmail.com>
2025-09-09 22:02:29 +0900

    #3.11 Directives

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
-      <div className="flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg">
-        <div className="group flex flex-col">
-          <input
-            className="w-full bg-gray-100"
-            placeholder="Write your Email"
-          />
-          <span className="hidden group-focus-within:block">
-            Make sure it is a valid email...
-          </span>
-          <button>Submit</button>
-        </div>
+    <main className="flex h-screen items-center justify-center bg-red-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+      <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
+        <input />
+        <a href="#">aaaa</a>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
