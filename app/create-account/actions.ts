54088d5467b70b11b1dcbfac08a9d0d6ba2fda9d
Jonghwa <whdghk3471@gmail.com>
2025-09-19 16:14:44 +0900

    #6.2 Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,12 +2,44 @@
 
 import z from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes("potato");
+
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        error: (issue) =>
+          issue.input === undefined
+            ? "Where is my username???"
+            : "Username must be a string!",
+      })
+      // invalid_type_error, required_error is deleted in string()
+      .min(3, "Way too short!!!")
+      /*     .min(3, {
+      error: (issue) => {
+        if (issue.code === "too_small") {
+          return `Value must be > ${issue.minimum}`;
+        }
+      },
+    }) */
+      .max(10, "That is too long!!!")
+      .refine(checkUsername, "No potatoes allowed!"),
+    email: z.email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswords, {
+    error: "Both passwords should be the same!",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -19,6 +51,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     const flatten = z.flattenError(result.error);
+    console.log(flatten);
     /*     const treeify = z.treeifyError(result.error);
     console.log(treeify); */
     return flatten;
