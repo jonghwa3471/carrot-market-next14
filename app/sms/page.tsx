240b244384e8d1b962f92a15694f027ed46bb047
Jonghwa <whdghk3471@gmail.com>
2025-09-23 22:07:06 +0900

    #6.9 Recap

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -30,6 +30,7 @@ export default function SMSLogIn() {
             required
             min={100000}
             max={999999}
+            errors={state.error?.formErrors}
             defaultValue=""
           />
         ) : (
