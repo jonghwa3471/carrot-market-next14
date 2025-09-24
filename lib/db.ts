54d70bb7682ca3b1e8d67a059b0e8a9c3e49ffb9
Jonghwa <whdghk3471@gmail.com>
2025-09-24 21:32:57 +0900

    #7.4 Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -2,4 +2,18 @@ import { PrismaClient } from "./generated/prisma";
 
 const db = new PrismaClient();
 
+async function test() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+
+test();
+
 export default db;
