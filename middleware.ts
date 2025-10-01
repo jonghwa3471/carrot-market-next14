aa814f21c375d814d83023f8c012bd48c46d488f
Jonghwa <whdghk3471@gmail.com>
2025-10-01 16:18:45 +0900

    #8.13 Recap

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
 
 /* const publicUrls = new Set(["/", "/login", "/sms", "/create-account"]);
