8771e5dabc041e708b2f16a07d911f317ce108bd
Jonghwa <whdghk3471@gmail.com>
2025-10-01 15:51:57 +0900

    #8.12 Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,46 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("hello");
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 }
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
+/* const publicUrls = new Set(["/", "/login", "/sms", "/create-account"]);
+
+export async function middleware(request: NextRequest) {
+  const isPublicPath = publicUrls.has(request.nextUrl.pathname);
+  const isLoggedIn = Boolean((await getSession()).id);
+
+  if (!isLoggedIn && !isPublicPath) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (isLoggedIn && isPublicPath) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+} */
