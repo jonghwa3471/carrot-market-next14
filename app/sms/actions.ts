1d638be0933ecbfe9ce6dd1bb4c29779f21dcbc3
Jonghwa <whdghk3471@gmail.com>
2025-10-16 13:23:23 +0900

    #9.7 Twilio SMS

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import twilio from "twilio";
 import crypto from "crypto";
 import z from "zod";
 import validator from "validator";
@@ -91,7 +92,16 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
           },
         },
       });
-      // send the token using twilio
+      const client = twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN,
+      );
+      await client.messages.create({
+        body: `Your Carrot verification code is: ${token}`,
+        from: process.env.TWILIO_PHONE_NUMBER!,
+        to: process.env.MY_PHONE_NUMBER!,
+        // to: result.data,
+      });
       return {
         token: true,
       };
