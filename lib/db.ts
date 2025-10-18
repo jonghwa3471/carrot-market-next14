98c7e9ab3b8af927e32ab608f5542e5332d2c81e
Jonghwa <whdghk3471@gmail.com>
2025-10-18 18:22:16 +0900

    #10.5 Product Detail

--- lib/db.ts
+++ lib/db.ts
@@ -2,18 +2,4 @@ import { PrismaClient } from "./generated/prisma";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 1,
-    },
-    include: {
-      user: true,
-    },
-  });
-  console.log("token:", token);
-}
-
-test();
-
 export default db;
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
 };
 
