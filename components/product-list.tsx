2705959070204a9c51318ebfba1a03ea7fa3119f
Jonghwa <whdghk3471@gmail.com>
2025-10-22 09:40:21 +0900

    #10.8 Recap

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -12,24 +12,35 @@ interface ProductListProps {
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
   const onLoadMoreClick = async () => {
     setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
     setIsLoading(false);
-    setProducts((prev) => [...prev, ...newProducts]);
   };
   return (
     <div className="flex flex-col gap-5 p-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        disabled={isLoading}
-        onClick={onLoadMoreClick}
-        className="mx-auto w-fit rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold hover:opacity-90 active:scale-95"
-      >
-        {isLoading ? "로딩 중" : "Load more"}
-      </button>
+      {isLastPage ? (
+        "No more items"
+      ) : (
+        <button
+          disabled={isLoading}
+          onClick={onLoadMoreClick}
+          className="mx-auto w-fit rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "Load more"}
+        </button>
+      )}
     </div>
   );
 }
